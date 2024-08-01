40cd57b998e5b1cd1d59be088bbd04a3bd4acb69
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2024-08-01 04:34:37 -0400

    Add files via upload

---
+++ Medal.js
@@ -0,0 +1,18 @@
+class Medal{
+    constructor(country, numGold, numSilver, numBronze) {
+        this.country = country;
+        this.numGold = numGold;
+        this.numSilver = numSilver;
+        this.numBronze = numBronze;
+        this.total = this.numGold + this.numSilver + this.numBronze;
+        this.number = 0;
+    }
+
+    toString() {
+        return `Gold: ${this.numGold} Silver: ${this.numSilver} Bronze: ${this.numBronze} Total: ${this.total}`;
+    }
+
+    static assignNumber(number) {
+        return number;
+    }
+}
--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 var sections;
 var list;
+var countries;
+var medals;
+var countriesCopy = [];
+var medalsDisplayed = false;
+
 //var sectionsArray = [];
 
 window.onload = () => {
     list = document.getElementsByClassName("nav-item");
     sections = document.getElementsByTagName("section");
+    countries = document.getElementsByClassName("medal-button");
+
     //console.log(`${list}\n${sections}`);
     console.log(list);
     console.log(sections);
+    createMedals();
+    for (var i = 0; i < countries.length; i++) {
+        countries[i].addEventListener("click", displayMedals);
+        countriesCopy.push(countries[i]);
+    }
     //document.addEventListener("click", onClick);
+}
+
+function createMedals() {
+    medals = [new Medal("China", 9, 7, 3), new Medal("France", 8, 10, 8)];
+}
+
+function displayMedals() {
+    var index = countriesCopy.indexOf(event.currentTarget);
+    if (!medalsDisplayed) {
+        medalsDisplayed.innerText = "Display Medals";
+        console.log(index);
+        countries[index].insertAdjacentText("afterend", medals[index].toString());
+    }
+    else {
+        event.currentTarget.innerText = "Hide Medals";
+        //document.getElementsByTagName("p")[index].style.display = "none";
+        console.log(document.getElementsByTagName("p")[index]);
+        //countries[index].removeChild(countries[index].children[0]);
+    }
+    medalsDisplayed = !medalsDisplayed;
 }
 /*
 function onClick() {
